e8cc58c63e13b10a40b3bb9b736fcf5abd899813
Anton <anton.tajudin@gmail.com>
2022-04-24 15:12:22 +0700

    ini rubah main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let h1 = document.querySelector('h1');
-const PI = 3.14I;
+const PI = 3.14;
 
 const BRAM = 'naon weh something tex in his';
-h1.innerHTML = "ini yg baru"
+h1.innerHTML = 'ini yg baru';
